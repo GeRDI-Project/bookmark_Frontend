88986d67a4224671a187610b63584dd8a75c2e9d
Nelson Tavares de Sousa <ntd@informatik.uni-kiel.de>
2018-09-18 17:17:17 +0200

    SAI-1138 fixed path to assets dir

--- config/index.js
+++ config/index.js
@@ -60,7 +60,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: 'bookmark/static',
     assetsPublicPath: '/',
 
     /**
